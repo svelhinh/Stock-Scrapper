507d8a97646832a1cdb809c86b1e540c2adf6aff
Sergio <s.velhinho@artgp.fr>
2020-11-28 22:30:57 +0100

    update config

--- lib/config.dart
+++ lib/config.dart
@@ -6,7 +6,7 @@ const TOPACHAT_RYZEN5_RYZEN7_URL =
 // const TOPACHAT_RTX3070_RTX3080_URL =
 //     "/pages/produits_cat_est_micro_puis_rubrique_est_wgfx_pcie_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_58-7624.html";
 // const TOPACHAT_RYZEN5_RYZEN7_URL =
-//     "/pages/produits_cat_est_m*icro_puis_rubrique_est_wpr_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_3-63%7C557-8660,8617.html";
+//     "/pages/produits_cat_est_micro_puis_rubrique_est_wpr_puis_ordre_est_P_puis_sens_est_ASC_puis_f_est_3-63%7C557-8660,8617.html";
 
 const BOULANGER_URL = 'https://www.boulanger.com';
 const BOULANGER_PS5_URL = "/ref/1147567";
